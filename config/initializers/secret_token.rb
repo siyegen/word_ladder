c34035c872ca56b5eea4f61cc6ff3cdd0c9bdca2
David Tomberlin <david.tomberlin@sendgrid.com>
2012-05-10 22:44:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordLadder::Application.config.secret_token = '8c9fc83046028bc6f04d63f498d33a7f2ab0fb1e2629d7fe1ae7a439d6ded73801ea9f92b0b55c23145ef1e4177dc1718ee182f00693ae96d434e82d3dc7c797'
