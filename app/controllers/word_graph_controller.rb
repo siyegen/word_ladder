3995b8171892bf95b938309e3ca7be3749a61b8a
David Tomberlin <david.tomberlin@sendgrid.com>
2012-06-05 15:20:23 -0700

    updated skeleton of controller

--- app/controllers/word_graph_controller.rb
+++ app/controllers/word_graph_controller.rb
@@ -1,10 +1,17 @@
 class WordGraphController < ApplicationController
+
+  # GET: Show last 100 solutions, or just the form if none
   def index
+
   end
 
-  def solve
+  # POST: Create a new solution for the given word
+  def create
+
   end
 
-  def solution
+  # GET: Show the solution
+  def show
+
   end
 end
