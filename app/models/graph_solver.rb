763a5a40ecd50c627a105a4a5203af69829b1754
David Tomberlin <david.tomberlin@sendgrid.com>
2012-06-05 12:03:00 -0700

    added graphsolver model first pass

---
+++ app/models/graph_solver.rb
@@ -0,0 +1,19 @@
+class GraphSolver
+  # Beginning of word graph to solver for
+  attr_accessor :start_word
+
+  # End of word graph we're trying to reach
+  attr_accessor :end_word
+
+  # Array of words to solve with
+  attr_accessor :words
+
+  # Solves shortest distance and returns 
+  # total_cost of moving
+  # start_word
+  # end_word
+  # path of nodes
+  def solve
+
+  end
+end
